8a38ea745087f16494ed9a8e8a792d57b7777eb5
jpca999 <jpca999@gmail.com>
2021-01-01 13:00:46 -0800

    put the backgound image as westaplm

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit 2 <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="bgimg"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
