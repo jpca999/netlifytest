d1e4687f791e2277f229d15e9b4bc698b90f4029
jpca999 <jpca999@gmail.com>
2021-01-01 13:12:05 -0800

    -

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit 2 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="bgimg"
@@ -13,7 +12,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
