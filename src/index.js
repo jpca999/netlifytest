69b8ba2a2547c6b4ab620655ce4f28620ff84943
jpca999 <jpca999@gmail.com>
2021-01-02 15:33:05 -0800

    hidden cookie message and minor updates

--- src/index.js
+++ src/index.js
@@ -15,3 +15,9 @@
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+
+
+// Save data to sessionStorage
+// sessionStorage.setItem('firstVisit', '1');
+
